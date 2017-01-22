1b57e9de6676a5c20266aecd8df658117b4194d8
shahar3 <shahar061@gmail.com>
2017-01-22 12:24:34 +0200

    commit

--- View/Controls/AddDateC.xaml.cs
+++ View/Controls/AddDateC.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PartnersMatcher.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,38 @@ namespace PartnersMatcher.View.Controls
     /// </summary>
     public partial class AddDateC : UserControl
     {
-        public AddDateC()
+        MyViewModel vm;
+
+        public AddDateC(MyViewModel vm)
         {
             InitializeComponent();
+            this.vm = vm;
+        }
+
+        /// <summary>
+        /// send the details to the model layer and insert them to the database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string address = addressTb.Text;
+            string budget = budgetTb.Text;
+            string city = cityCb.SelectedItem.ToString();
+            bool alcohol = alcoholCb.IsChecked.Value;
+            bool smoking = smokingCb.IsChecked.Value;
+            string description = descTb.Text;
+            int num;
+            if(!Int32.TryParse(budget,out num))
+            {
+                MessageBox.Show("Budget must to be a number");
+                return;
+            }
+            if (address == "")
+            {
+                MessageBox.Show("You didn't write the date address");
+                return;
+            }
         }
     }
 }
